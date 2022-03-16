c129cf8d5ad911a8609f2f3af72a936e0789432b
priorityc <petya83dimitrova@gmail.com>
2022-03-16 14:13:58 +0000

    added Mobile hamburger menu whith JS

---
+++ appportfolio.js
@@ -0,0 +1,10 @@
+Newmenu = (e)=> {
+  document.querySelector('i').innerHTML=`<ul>
+  <li><a href="#" class="navlinks">Deals</a></li>
+  <li><a href="#" class="navlinks">Locations</a></li>
+  <li><a href="#" class="navlinks">Help</a></li>
+  <li><a href="#" class="navlinks">Contact</a></li>
+</ul>`
+}
+document.addEventListener('click', Newmenu);
+
